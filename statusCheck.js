8277573c0ee81331ca65f221acc02550ec6d3f49
Ben Bahrman <ben@bahrman.org>
2017-12-27 21:48:45 -0800

    Removing unused code

--- statusCheck.js
+++ statusCheck.js
@@ -1,7 +1,4 @@
 var nodegit = require('nodegit');
-var Linter = require('eslint').Linter;
-var linter = new Linter();
-var esprima = require('esprima');
 var fs = require('fs');
 var directory = '/Users/benbahrman/Code_Review';
 // This code shows working directory changes similar to git status
@@ -59,80 +56,3 @@ function lintFilesSimple (fileArray) {
         });
     });
 }
-
-
-function lintFiles (fileArray) {
-    const SourceCode = require('eslint').SourceCode;
-    fileArray.forEach(function (file) {
-        fs.readFile(directory + '/' + file, 'utf8', function (err, data) {
-            if(err) {
-                console.log('Error ' + err);
-            } else {
-                var token = esprima.parseScript(data,
-                    {
-                    tokens:true,
-                    comment: true,
-                    loc: true,
-                    range: true
-                    });
-                var code = new SourceCode(data, token);
-                var messages = linter.verifyAndFix(code, {
-                 'rules': rules
-                 }, {filename: file });
-
-            }
-        });
-    });
-}
-
-
-
-var rules = {
-    "constructor-super": 2,
-    "no-case-declarations": 2,
-    "no-class-assign": 2,
-    "no-compare-neg-zero": 2,
-    "no-cond-assign": 2,
-    "no-console": 2,
-    "no-const-assign": 2,
-    "no-constant-condition": 2,
-    "no-control-regex": 2,
-    "no-debugger": 2,
-    "no-delete-var": 2,
-    "no-dupe-args": 2,
-    "no-dupe-class-members": 2,
-    "no-dupe-keys": 2,
-    "no-duplicate-case": 2,
-    "no-empty-character-class": 2,
-    "no-empty-pattern": 2,
-    "no-empty": 2,
-    "no-ex-assign": 2,
-    "no-extra-boolean-cast": 2,
-    "no-extra-semi": 2,
-    "no-fallthrough": 2,
-    "no-func-assign": 2,
-    "no-global-assign": 2,
-    "no-inner-declarations": 2,
-    "no-invalid-regexp": 2,
-    "no-irregular-whitespace": 2,
-    "no-mixed-spaces-and-tabs": 2,
-    "no-new-symbol": 2,
-    "no-obj-calls": 2,
-    "no-octal": 2,
-    "no-redeclare": 2,
-    "no-regex-spaces": 2,
-    "no-self-assign": 2,
-    "no-sparse-arrays": 2,
-    "no-this-before-super": 2,
-    "no-undef": 2,
-    "no-unexpected-multiline": 2,
-    "no-unreachable": 2,
-    "no-unsafe-finally": 2,
-    "no-unsafe-negation": 2,
-    "no-unused-labels": 2,
-    "no-unused-vars": 2,
-    "no-useless-escape": 2,
-    "require-yield": 2,
-    "use-isnan": 2,
-    "valid-typeof": 2
-};
