807e3bb8d9afca912980e80a299c39433ed2b178
Ben Bahrman <ben@bahrman.org>
2018-01-15 06:15:49 -0800

    Fetch is finally working, need to clean up remainder of file, implement better logging and figure out merge conflicts

--- lintAndCommit.js
+++ lintAndCommit.js
@@ -5,36 +5,87 @@ var signature;
 var parentCommit;
 var repositoryObj;
 var changedFiles;
+let branchName;
+let fetchStatus;
 // This code shows working directory changes similar to git status
 let commitPromise = getCommitMessage();
 
 nodegit.Repository.open(directory).then(function (repo) {
     repositoryObj = repo;
-
+    repositoryObj.getHeadCommit().then(function (headCommit) {
+        parentCommit = headCommit;
+        branchName = getBranchName();
+        fetchStatus = fetchBranches();
+    });
     Promise.all([
         processFiles(),
         commitPromise
     ])
     .then(function (results) {
+        console.log('Process files and commit promise resolved');
         // commit changes, last three arguments are OID, commit message, and parent commit
-        repositoryObj.createCommit('HEAD', signature, signature,  results[1], results[0], [parentCommit]);
+        let commit = repositoryObj.createCommit('HEAD', signature, signature,  results[1], results[0], [parentCommit]);
+        Promise.all([
+            commit,
+            branchName,
+            fetchStatus
+        ]).then((results)=>{
+            console.log('fetch complete, branch: ' + results[1]);
+            // merge remote branch in
+            return repositoryObj.mergeBranches(results[1], 'origin/' + results[1], signature);
+        }).catch((err) => {
+            console.log('Error in promise block commit, branchname, fetch');
+            console.log(err);
+        });
+    })
+    .catch(function (err) {
+        console.log('Error in promise block, processFules and commitPromise\n' + err)
     });
 });
 
+function getBranchName () {
+    return new Promise((resolve, reject) => {
+        try {
+            repositoryObj.getBranch('HEAD').then(function (reference) {
+                resolve(reference.name().replace('refs/heads/', ''))
+            });
+        } catch (err) {
+            reject('Error getBranchName: ' + err);
+        }
+    });
+}
+
+function fetchBranches () {
+    return new Promise((resolve, reject) => {
+       try {
+           repositoryObj.fetch("origin",{
+               callbacks: {
+                   certificateCheck: function() { return 1; },
+                   credentials: function(url, userName) {
+                       return nodegit.Cred.sshKeyFromAgent(userName);
+                   }
+               }
+               },(success) => {
+                   console.log('fetchBranches result: ' + success);
+                   resolve(true);
+           });
+       } catch (err) {
+           reject('Error fetchBranches: ' + err);
+       }
+    });
+}
 
 // Return OID for commit and set repository obj on global scale
 function processFiles(){
     return new Promise(function (resolve, reject) {
-        genChangedFiles(repositoryObj)
-            .then(lintFilesSimple)
-            .then(guidedCommit)
-            .then(function (oid) {
+        try {
+            genChangedFiles(repositoryObj).then(lintFilesSimple).then(guidedCommit).then(function (oid) {
                 resolve(oid);
-        });
-        repositoryObj.getHeadCommit().then(function (headCommit) {
-            parentCommit = headCommit;
-        });
-        signature = nodegit.Signature.default(repositoryObj);
+            });
+            signature = nodegit.Signature.default(repositoryObj);
+        } catch (err) {
+            reject('Error processFiles: ' + err);
+        }
     });
 }
 
@@ -44,7 +95,7 @@ function createSignature () {
            if(signature){resolve (signature)}
            else (signature.now())
        } catch (err) {
-           reject(err);
+           reject('Error createSignature: ' + err);
        }
     });
 }
@@ -60,49 +111,57 @@ function isChanged (fileObj) {
 
 function genChangedFiles(repo) {
     return new Promise(function (genChangedFiles_resolve, genChangedFiles_reject) {
-        repo.getStatus().then(function(statuses) {
-            var files = [];
-            var itemsProcessed = 0;
+        try {
+            repo.getStatus().then(function (statuses) {
+                var files = [];
+                var itemsProcessed = 0;
 
-            statuses.forEach(function(file) {
-                if (isChanged(file)) {
-                    files.push(directory + '/' + file.path());
-                    itemsProcessed++;
-                    //console.log(itemsProcessed + ' files = ' + files.join(','));
-                }
-                if(itemsProcessed === statuses.length){
-                    changedFiles = files;
-                    genChangedFiles_resolve();
-                }
+                statuses.forEach(function (file) {
+                    if (isChanged(file)) {
+                        files.push(directory + '/' + file.path());
+                        itemsProcessed++;
+                        //console.log(itemsProcessed + ' files = ' + files.join(','));
+                    }
+                    if (itemsProcessed === statuses.length) {
+                        changedFiles = files;
+                        genChangedFiles_resolve();
+                    }
+                });
             });
-        });
+        } catch (err) {
+            genChangedFiles_reject('Error genChangedFiles: ' + err);
+        }
     });
 }
 
 function lintFilesSimple () {
     return new Promise(function (lintFiles_resolve, lintFiles_reject) {
-        var CLIEngine = require("eslint").CLIEngine;
+        try {
+            var CLIEngine = require("eslint").CLIEngine;
 
-        var cli = new CLIEngine({
-            useEslintrc: true, fix: true
-        });
-        // lint myfile.js and all files in lib/
-        var report = cli.executeOnFiles(changedFiles);
-        var filesProcessed = 0;
-        report.results.forEach(function (fileResponse) {
-            if(!isEmptyOrNull(fileResponse.output)) {
-                fs.writeFile(fileResponse.filePath, fileResponse.output, function (err) {
-                    if (err) {
-                    }
-                    filesProcessed++;
-                    if (filesProcessed === report.results.length) {
-                        lintFiles_resolve();
-                    }
-                });
-            } else {
-                lintFiles_resolve();
-            }
-        });
+            var cli = new CLIEngine({
+                useEslintrc: true, fix: true
+            });
+            // lint myfile.js and all files in lib/
+            var report = cli.executeOnFiles(changedFiles);
+            var filesProcessed = 0;
+            report.results.forEach(function (fileResponse) {
+                if (!isEmptyOrNull(fileResponse.output)) {
+                    fs.writeFile(fileResponse.filePath, fileResponse.output, function (err) {
+                        if (err) {
+                        }
+                        filesProcessed++;
+                        if (filesProcessed === report.results.length) {
+                            lintFiles_resolve();
+                        }
+                    });
+                } else {
+                    lintFiles_resolve();
+                }
+            });
+        } catch (err) {
+            lintFiles_reject('Error lintFilesSimple: ' + err);
+        }
     });
 }
 
@@ -112,30 +171,34 @@ function isEmptyOrNull (testVar) {
     }
     return false;
 }
+
 function guidedCommit () {
     return new Promise(function (guidedCommit_resolve, guidedCommit_reject) {
-        repositoryObj.refreshIndex()
-        .then(function (index) {
-            var filesAdded = 0;
-            changedFiles.forEach(function (file) {
-                index.addByPath(file.replace(directory + '/','')).then(function (result) {
-                    filesAdded++;
-                    if(filesAdded === changedFiles.length) {
-                        index.write().then(function(){
-                            return index.writeTree();
-                        }).then(function (oid) {
-                            guidedCommit_resolve(oid)
-                        });
-                    }
-                })
+        try {
+            repositoryObj.refreshIndex().then(function (index) {
+                var filesAdded = 0;
+                changedFiles.forEach(function (file) {
+                    index.addByPath(file.replace(directory + '/', '')).then(function (result) {
+                        filesAdded++;
+                        if (filesAdded === changedFiles.length) {
+                            index.write().then(function () {
+                                return index.writeTree();
+                            }).then(function (oid) {
+                                guidedCommit_resolve(oid)
+                            });
+                        }
+                    })
+                });
             });
-        });
+        } catch (err) {
+            guidedCommit_reject('Error guidedCommit: ' + err);
+        }
     });
 }
 
 function getCommitMessage () {
     return new Promise(function (resolve, reject) {
-        try {
+        /*try {
             const readline = require('readline');
             const rl = readline.createInterface({
                 input: process.stdin, output: process.stdout
@@ -145,7 +208,8 @@ function getCommitMessage () {
                 resolve(answer)
             });
         } catch (err) {
-            reject(err)
-        }
+            reject('Error getCommitMessage: ' + err)
+        }*/
+        resolve('fake commit');
     });
 }
